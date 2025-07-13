e910ba38aa8aa051d7ed3c1fe90b6b744e2d7829
KaVin S <kavinadithya3@gmail.com>
2025-07-14 04:54:57 +0530

    Developer Log project setup completed

---
+++ src/main/java/com/techcrack/devlog/DevLogsApplication.java
@@ -0,0 +1,13 @@
+package com.techcrack.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcrack/devlog/DevLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcrack.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
